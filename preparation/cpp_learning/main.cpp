5ce06be08bc3d6de4bcfddd463925e3927ff4480
FaxWeb <80632493+FaxWeb7@users.noreply.github.com>
2023-12-24 18:57:48 +0300

    34 lessons

--- preparation/cpp_learning/main.cpp
+++ preparation/cpp_learning/main.cpp
@@ -342,10 +342,350 @@
 
 
 //------------ WHILE LOOPS ------------//
+// #include <iostream>
+
+// int main(){
+//     std::string name;
+//     while (name.empty()){
+//         std::cout << "Enter your name: ";
+//         std::getline(std::cin, name);
+//     }
+//     std::cout << "Hello " << name;
+
+//     return 0;
+// }
+
+
+//------------ DO WHILE LOOPS ------------//
+// #include <iostream>
+
+// int main(){
+//     // do while loop = do some block of code first, THEN repeat again if condition is true.
+//     int number;
+
+//     do{
+//         std::cout << "Enter a positive number: ";
+//         std::cin >> number;
+//     } while (number < 0);
+
+//     std::cout << "The number is " << number;
+
+//     return 0;
+// }
+
+
+//------------ FOR LOOPS ------------//
+// #include <iostream>
+
+// int main(){
+//     for (int i = 5; i > 0; i--){
+//         std::cout << i << std::endl;
+//     }
+//     std::cout << "HAPPY NEW YEAR!";
+
+//     return 0;
+// }
+
+
+//------------ BREAK & CONTINUE ------------//
+// #include <iostream>
+
+// int main(){
+//     // break = break out of loop
+//     // continue = skip current iteration
+
+//     for (int i = 1; i <= 20; i++){
+//         if (i == 13){
+//             // continue;
+//             break;
+//         }
+//         std::cout << i << std::endl;
+//     }
+
+//     return 0;
+// }
+
+
+//------------ NESTED LOOPS ------------//
+// #include <iostream>
+
+// int main(){
+//     int rows;
+//     int cols;
+//     char symbol;
+
+//     std::cout << "Enter # of rows: ";
+//     std::cin >> rows;
+//     std::cout << "Enter # of columns: ";
+//     std::cin >> cols;
+//     std::cout << "Enter symbol: ";
+//     std::cin >> symbol;
+
+//     for (int i = 0; i < rows; i++){
+//         for (int j = 0; j < cols; j++){
+//             std::cout << symbol << ' ';
+//         }
+//         std::cout << '\n';
+//     }
+
+//     return 0;
+// }
+
+
+//------------ RANDOM NUMBERS  ------------//
+// #include <iostream>
+
+// int main(){
+//     // pseudo random = NOT truly random (but close)
+
+//     srand(time(NULL));
+//     int num = (rand() % 100) + 1;
+
+//     std::cout << num << std::endl;
+
+//     return 0;
+// }
+
+
+//------------ NUMBER GUESSING GAME ------------//
+// #include <iostream>
+
+// int main(){
+//     int number;
+//     int guess;
+//     int tries;
+
+//     srand(time(NULL));
+//     number = (rand() % 100) + 1;
+//     tries = 0;
+
+//     do{
+//         std::cout << "Enter a guess between 1-100: ";
+//         std::cin >> guess;
+//         tries++;
+//         if (number < guess){
+//             std::cout << "Too high!\n";
+//         }
+//         else{
+//             std::cout << "Too low!\n";
+//         }
+//     } while (number != guess);
+
+//     std::cout << "CORRECT! # of tries: " << tries;
+
+//     return 0;
+// }
+
+
+//------------ FUNCTIONS ------------//
+// #include <iostream>
+
+// double countArea(double length);
+
+// int main(){
+//     // function = block of reusable code  
+//     double length;
+//     double area;
+
+//     std::cout << "Enter length (cm): ";
+//     std::cin >> length;
+
+//     area = countArea(length);
+//     std::cout << "Area: " << area << " cm2";
+
+//     return 0;
+// }
+
+// double countArea(double length){
+//     return length * length;
+// }
+
+
+//------------ OVERLOADED FUNCTIONS ------------//
+// #include <iostream>
+
+// void bakePizza();
+// void bakePizza(std::string topping);
+
+// int main(){
+//     bakePizza();
+//     bakePizza("pepperoni");
+//     return 0;
+// }
+
+// void bakePizza(){
+//     std::cout << "Here is your pizza!\n";
+// }
+// void bakePizza(std::string topping){
+//     std::cout << "Here is your " << topping << " pizza!\n";
+// }
+
+
+//------------ BANKING PROGRAM ------------//
+// #include <iostream>
+// using namespace std;
+
+// void showBalance(double balance);
+// double deposit();
+// double withdraw();
+
+// int main(){
+//     double balance = 100;
+//     int choice;
+
+//     do{
+//         cout << "Enter your choice (1-4):\n";
+//         cout << "1. Show balance\n";
+//         cout << "2. Deposit money\n";
+//         cout << "3. Withdraw money\n";
+//         cout << "4. Exit\n";
+//         cin >> choice;
+//         switch(choice){
+//             case 1:
+//                 showBalance(balance);
+//                 break;
+//             case 2:
+//                 balance += deposit();
+//                 showBalance(balance);
+//                 break;
+//             case 3:
+//                 balance -= withdraw();
+//                 showBalance(balance);
+//                 break;
+//         }
+//     } while (choice != 4);
+//     cout << "Thanks for using our bank!";
+
+//     return 0;
+// }
+
+// void showBalance(double balance){
+//     cout << "Your balance: " << balance << "$\n\n";
+// }
+// double deposit(){
+//     double deposit = 0;
+//     cout << "Enter how much do you want to deposit: ";
+//     cin >> deposit;
+//     cout << "You successfully deposit " << deposit << "$\n";
+//     return deposit;
+// }
+// double withdraw(){
+//     double withdraw;
+//     cout << "Enter how much do you want to withdraw: ";
+//     cin >> withdraw;
+//     cout << "You successfully withdraw " << withdraw << "$\n";
+//     return withdraw;
+// }
+
+
+//------------ ROCK PAPER SCISSORS GAME ------------//
+// #include <iostream>
+// using namespace std;
+
+// string convertChoice(char choice);
+// string winnerText(char playerChoice, char computerChoice);
+
+// int main(){
+//     srand(time(NULL));
+//     char player_choice;
+//     char computer_choice;
+//     int randomNumber = (rand() % 3) + 1;
+
+//     computer_choice = randomNumber == 1 ? 'r' : randomNumber == 2 ? 'p' : 's';
+//     cout << "Choose one of the following\n";
+//     cout << "*************************\n";
+//     cout << "'r' - for rock\n";
+//     cout << "'p' - for paper\n";
+//     cout << "'s' - for scissors\n";
+//     cin >> player_choice;
+
+//     cout << "Your choice: " << convertChoice(player_choice) << endl;
+//     cout << "Computer's choice: " << convertChoice(computer_choice) << endl;
+//     cout << winnerText(player_choice, computer_choice) << endl;
+
+//     return 0;
+// }
+
+// string convertChoice(char choice){
+//     string fullChoice;
+//     switch(choice){
+//         case 'r':
+//             fullChoice = "Rock";
+//             break;
+//         case 'p':
+//             fullChoice = "Paper";
+//             break;
+//         case 's':
+//             fullChoice = "Scissors";
+//             break;
+//     }
+//     return fullChoice;
+// }
+
+// string winnerText(char playerChoice, char computerChoice){
+//     string winnerText;
+//     if (playerChoice == computerChoice){
+//         winnerText = "It's a tie!";
+//     }
+//     else if ((playerChoice == 'r' && computerChoice == 's') || (playerChoice == 's' && computerChoice == 'p') || (playerChoice == 'p' && computerChoice == 'r')){
+//         winnerText = "You win!";
+//     }
+//     else{
+//         winnerText = "You lose :(";
+//     }
+//     return winnerText;
+// }
+
+
+//------------ ARRAYS ------------//
+// #include <iostream>
+
+// int main(){
+//     // std::string cars[] = {"Mustang", "Corvette", "BMW"};
+//     std::string cars[3];
+//     cars[0] == "Mustang";
+//     cars[1] == "Corvette";
+//     cars[2] == "BMW";
+
+//     std::cout << cars[0] << std::endl;
+//     std::cout << cars[1] << std::endl;
+//     std::cout << cars[2] << std::endl;
+
+//     return 0;
+// }
+
+
+//------------ SIZEOF OPERATOR ------------//
+// #include <iostream>
+
+// int main(){
+//     // sizeof() = determines the size in bytes of:
+//     //          variable, data type, class, object, etc.
+
+//     int age = 16;
+//     std::string name = "Artem";
+//     double gpa = 2.5;
+//     char grade = 'F';
+//     bool student = true;
+//     std::string students[] = {"Patrick", "SpongeBob", "Squidward"};
+
+
+//     std::cout << "int - " << sizeof(age) << " bytes\n";
+//     std::cout << "string - " << sizeof(name) << " bytes\n";
+//     std::cout << "double - " << sizeof(gpa) << " bytes\n";
+//     std::cout << "char - " << sizeof(grade) << " bytes\n";
+//     std::cout << "boolean - " << sizeof(student) << " bytes\n";
+//     std::cout << sizeof(students)/sizeof(students[0]) << " elements in array";
+
+//     return 0;
+// }
+
+
+//------------ ITERATE OVER IN ARRAY ------------//
 #include <iostream>
 
-int main(){
-
+int main(){ 
+    
 
     return 0;
 }
